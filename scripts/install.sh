382fad97b07832b7741edf3830a6f1d5d0500ade
YAMAMOTO Takashi <yamamoto@midokura.com>
2023-02-15 09:14:37 +0900

    use wasi-sdk-20

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-WASI_VERSION=17
-WASI_VERSION_FULL=${WASI_VERSION}.7gafa685ef7a50
-wget https://github.com/yamt/wasi-sdk/releases/download/yamt-20221229/wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
+WASI_VERSION=20
+WASI_VERSION_FULL=${WASI_VERSION}.0
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
 tar xvf wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
 rm wasi-sdk-${WASI_VERSION_FULL}-linux.tar.gz
 mv wasi-sdk-${WASI_VERSION_FULL} wasi-sdk
